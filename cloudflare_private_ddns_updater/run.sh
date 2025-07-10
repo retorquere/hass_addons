a53e9805bbaa74f79566cc7b7a1e561f990fa2be
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-07-10 03:42:51 +0200

    update

--- cloudflare_private_ddns_updater/run.sh
+++ cloudflare_private_ddns_updater/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
 python3 makeconfig.py
-cat etc/ddclient/ddclient.conf
+cat /etc/ddclient/ddclient.conf
+chown root:root /etc/ddclient/ddclient.conf
+echo =====
 ip -6 addr show scope global
+echo =====
 ddclient -foreground -verbose -debug
 
