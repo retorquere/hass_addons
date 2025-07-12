d0109ec39bbc3612f6ea9a3fda3488b9990af3a3
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-07-12 23:51:35 +0200

    update

--- cloudflare_private_ddns_updater/makeconfig.py
+++ cloudflare_private_ddns_updater/makeconfig.py
@@ -5,14 +5,31 @@ import netifaces
 import ipaddress
 from types import SimpleNamespace
 
-def get_gua():
+def get_ips():
+  ipv4 = None
+  ipv6 = None
+
   for interface_name in netifaces.interfaces():
+    if ipv4 and ipv6: break
+
     try:
       addresses = netifaces.ifaddresses(interface_name)
-      if netifaces.AF_INET6 not in addresses: continue
+    except ValueError:
+      # netifaces.ifaddresses can raise ValueError for non-existent interfaces
+      continue
 
-      for addr_info in addresses[netifaces.AF_INET6]:
-        ip_str = addr_info.get('addr')
+    if not ipv4 and netifaces.AF_INET in addresses:
+      for link in addresses[netifaces.AF_INET]:
+        ip_str = link.get('addr')
+        if not ip_str: continue
+
+        if not ip_str.startswith('127.') and ip_str != '0.0.0.0':
+          ipv4 = ip_str
+          break
+
+    if not ipv6 and netifaces.AF_INET6 in addresses:
+      for link in addresses[netifaces.AF_INET6]:
+        ip_str = link.get('addr')
         if not ip_str: continue
 
         # Clean up any scope ID suffix (e.g., %eth0)
@@ -21,28 +38,29 @@ def get_gua():
         try:
           ip_obj = ipaddress.ip_address(ip_str)
           if ip_obj.is_global:
-            return str(ip_obj)
+            ipv6 = str(ip_obj)
+            break
         except ipaddress.AddressValueError:
           # Not a valid IP address string, skip
           continue
-    except ValueError:
-      # netifaces.ifaddresses can raise ValueError for non-existent interfaces
-      pass
+
+  return (ipv4, ipv6)
 
 with open('/etc/ddclient/ddclient.conf', 'w') as f:
-  f.write(f'''
-daemon=300
-syslog=yes
-pid=/run/ddclient/ddclient.pid
-ip={get_gua()}
-''')
-  for config in json.load(open('/data/options.json'), object_hook=lambda x: SimpleNamespace(**x)).config:
+  ipv4, ipv6 = get_ips()
 
-    f.write(f'''
-protocol=cloudflare
-zone={config.zone}
-ttl=1
-login=token
-password={config.token}
-{config.domains}
-''')
+  f.write('daemon=300\n')
+  f.write('syslog=yes\n')
+  f.write('ssl=yes\n')
+  f.write('pid=/run/ddclient/ddclient.pid\n')
+
+  for config in json.load(open('/data/options.json'), object_hook=lambda x: SimpleNamespace(**x)).config:
+    f.write('\nprotocol=cloudflare\n')
+    f.write(f'zone={config.zone}\n')
+    f.write('ttl=1\n')
+    f.write('login=token\n')
+    f.write(f'password={config.token}\n')
+    f.write('use=static\n')
+    f.write(f'ip={ipv4}\n' if ipv4 else 'usev4=no\n')
+    f.write(f'ip6={ipv6}\n' if ipv6 else 'usev6=no\n')
+    f.write(f'{config.domains}\n')
