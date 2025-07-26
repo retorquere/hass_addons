ef687ad2ae498df2a996b1c4e36a746c0584273a
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-07-27 00:34:41 +0200

    update

--- cloudflare_private_ddns_updater/makeconfig.py
+++ cloudflare_private_ddns_updater/makeconfig.py
@@ -24,7 +24,7 @@ def get_ips():
         if not ip_str: continue
 
         if not ip_str.startswith('127.') and ip_str != '0.0.0.0':
-          ipv4 = ip_str
+          ipv4 = f'ip, ip={ip_str}'
           break
 
     if not ipv6 and netifaces.AF_INET6 in addresses:
@@ -38,13 +38,13 @@ def get_ips():
         try:
           ip_obj = ipaddress.ip_address(ip_str)
           if ip_obj.is_global:
-            ipv6 = str(ip_obj)
+            ipv6 = f'ipv6, ipv6={ip_obj}'
             break
         except ipaddress.AddressValueError:
           # Not a valid IP address string, skip
           continue
 
-  return (ipv4, ipv6)
+  return (ipv4 or 'no', ipv6 or 'no')
 
 with open('/etc/ddclient/ddclient.conf', 'w') as f:
   ipv4, ipv6 = get_ips()
@@ -55,12 +55,11 @@ with open('/etc/ddclient/ddclient.conf', 'w') as f:
   f.write('pid=/run/ddclient/ddclient.pid\n')
 
   for config in json.load(open('/data/options.json'), object_hook=lambda x: SimpleNamespace(**x)).config:
-    for v, ip in [('', ipv4), ('v6', ipv6)]:
-      if ip:
-        f.write('\nprotocol=cloudflare\n')
-        f.write(f'zone={config.zone}\n')
-        f.write('ttl=1\n')
-        f.write('login=token\n')
-        f.write(f'password={config.token}\n')
-        f.write(f'use{v}=ip{v}, ip{v}={ip}\n')
-        f.write(f'{config.domains}\n\n')
+    f.write('\nprotocol=cloudflare\n')
+    f.write(f'zone={config.zone}\n')
+    f.write('ttl=1\n')
+    f.write('login=token\n')
+    f.write(f'password={config.token}\n')
+    f.write(f'usev4={ipv4}\n')
+    f.write(f'usev6={ipv6}\n')
+    f.write(f'{config.domains}\n\n')
